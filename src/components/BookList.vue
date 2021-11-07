2dae4c2adff62829f6be50c2f916cadd09b3b9e8
Lucas Gladding <lucas@me.com>
2021-11-07 11:45:04 -0500

    Added books header.

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,10 +1,10 @@
 <template>
-  <div v-if="loading">
-    Loading...
-  </div>
-  <div v-else-if="result && result.books">
-    <book-list-item v-for="book in result.books" :book="book" :key="book.title" class="p-4 border-b" />
-  </div>
+  <section class="p-4">
+    <h1 class="text-4xl mb-4">Books</h1>
+    <div v-if="result && result.books">
+      <book-list-item v-for="book in result.books" :book="book" :key="book.title" class="py-4 border-b" />
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
