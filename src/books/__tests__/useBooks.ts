4a80a6156b6ff6e727158c452ac6f9701cfdabb1
Lucas Gladding <lucas@me.com>
2021-11-07 21:28:29 -0500

    Added test for books composable.

--- src/books/__tests__/useBooks.ts
+++ src/books/__tests__/useBooks.ts
@@ -1,7 +1,25 @@
+import { provideApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, ApolloLink, InMemoryCache, Observable } from '@apollo/client/core'
 import useBooks from '@/books/useBooks'
 
+const wait = (timeout = 0) => new Promise(resolve => setTimeout(resolve, timeout))
+
 describe('useBooks', () => {
-  it('lists books', () => {
-    expect(true).toEqual(true)
+  it('lists books', async () => {
+    const response = {
+      data: { books: [] }
+    }
+    const link = new ApolloLink(() => {
+      return Observable.of(response)
+    })
+    const cache = new InMemoryCache()
+    const client = new ApolloClient({
+      link,
+      cache
+    })
+    provideApolloClient(client)
+    const { result } = useBooks()
+    await wait()
+    expect(result.value).toEqual(response.data)
   })
 })
--- src/books/useBooks.ts
+++ src/books/useBooks.ts
@@ -2,17 +2,19 @@ import { useQuery, UseQueryReturn } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 import Book from '@/books/Book'
 
+export const LIST_BOOKS_QUERY = gql`
+  query ListBooks {
+    books {
+      title,
+      author
+    }
+  }
+`
+
 interface Result {
   books: Book[]
 }
 
 export default function useBooks (): UseQueryReturn<Result, undefined> {
-  return useQuery<Result>(gql`
-    query listBooks {
-      books {
-        title,
-        author
-      }
-    }
-  `)
+  return useQuery<Result>(LIST_BOOKS_QUERY)
 }
